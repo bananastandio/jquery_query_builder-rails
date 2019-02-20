7c6e2595371a94dacde8242be81c6b7e70686d6a
Lucas Arantes <lucas.ruby.dev@gmail.com>
2019-02-20 12:45:31 -0500

    Return false when left is nil on NotContains

--- lib/jquery_query_builder/operators/not_contains.rb
+++ lib/jquery_query_builder/operators/not_contains.rb
@@ -2,6 +2,7 @@ module JqueryQueryBuilder
   module Operators
     class NotContains
       def evaluate(left, right)
+        return false if left.nil?
         !left.include?(right)
       end
     end
