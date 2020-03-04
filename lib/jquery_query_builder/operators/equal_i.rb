000c3fafc567afd5a61d26a4ecf1964f3c75a966
Jay El-Kaake <najibkaake@gmail.com>
2020-03-04 18:05:37 -0500

    Add failsafes for equal_i and equal when the left side is not provided.

--- lib/jquery_query_builder/operators/equal_i.rb
+++ lib/jquery_query_builder/operators/equal_i.rb
@@ -2,6 +2,8 @@ module JqueryQueryBuilder
   module Operators
     class EqualI
       def evaluate(left, right)
+        return false if left.nil? # If the thing we're looking at is not defined, assume it's not equal
+        return true if left == right # If they're totally equal, even before assuming it's a string, then they're also case-insenstive equal
         left.casecmp(right) == 0
       end
     end
--- lib/jquery_query_builder/operators/not_equal_i.rb
+++ lib/jquery_query_builder/operators/not_equal_i.rb
@@ -2,6 +2,8 @@ module JqueryQueryBuilder
   module Operators
     class NotEqualI
       def evaluate(left, right)
+        return false if left.nil? && !right.nil? # If the thing we're looking at is not defined, assume it's not not equal
+        return false if left == right # If they're totally equal, even before assuming it's a string, then they're also not case-insenstive equal
         left.casecmp(right) != 0
       end
     end
